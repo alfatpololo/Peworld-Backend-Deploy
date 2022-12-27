b8a16489a3f2911def003c76aa831ae47bfb9888
unknown <alfat.pololo@gmail.com>
2022-12-27 14:37:21 +0700

    fix update users3

--- src/middleware/upload_user.js
+++ src/middleware/upload_user.js
@@ -7,9 +7,7 @@ const path = require('path');
 //management file
 const multerUpload = multer ({
     storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, './public')
-        },
+        
     filename: (req, file, cb) => {
         const ext = path.extname(file.originalname);
 
